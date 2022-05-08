5ec43825801999313e52b3309161ef74e0ef15ac
Dania <daniatub8@gmail.com>
2022-05-08 11:28:14 +0300

    Initial commit

---
+++ src/ProblemSolving/One.java
@@ -0,0 +1,29 @@
+package ProblemSolving;
+
+public class One
+{
+
+        public int[] plusOne(int[] digits)
+        {
+            int size=digits.length;
+
+            for (int i = size-1; i >=0; i--)
+            {
+                if(digits[i]<9)
+                {
+                    digits[i]++;
+                    return digits;
+                }
+
+                digits[i]=0;
+                //add new element
+
+            }
+            int[] newdigits=new int[size+1];
+
+            digits[0]=1;
+            return  newdigits;
+
+
+        }
+    }
