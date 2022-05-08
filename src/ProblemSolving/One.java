71852bc1e1ebedc9de6ef29c3bd571899bf8f6dc
Dania <daniatub8@gmail.com>
2022-05-08 12:16:02 +0300

    Plus One ..

--- src/ProblemSolving/One.java
+++ src/ProblemSolving/One.java
@@ -2,25 +2,19 @@ package ProblemSolving;
 
 public class One
 {
-
         public int[] plusOne(int[] digits)
         {
             int size=digits.length;
-
-            for (int i = size-1; i >=0; i--)
+            for (int i = size-1; i>=0; i--)
             {
                 if(digits[i]<9)
                 {
                     digits[i]++;
                     return digits;
                 }
-
                 digits[i]=0;
-                //add new element
-
             }
             int[] newdigits=new int[size+1];
-
             digits[0]=1;
             return  newdigits;
 
